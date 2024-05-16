d038aa70744a9458d87226025f17bbd625ee1b2b
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-05-16 14:54:53 +0800

    (feat): Nav + Header

---
+++ src/components/PageNav.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <nav>
+    <tr>
+      <th><RouterLink to="/seasons">Seasons</RouterLink></th>
+      <th><RouterLink to="/teams">Teams</RouterLink></th>
+      <th><RouterLink to="/games">Games</RouterLink></th>
+      <th><RouterLink to="/players">Players</RouterLink></th>
+    </tr>
+  </nav>
+</template>
+
+<style scoped lang="sass">
+nav
+  tr
+    padding-inline: 0.5em
+    th
+      transition: linear 0.1s
+      cursor: pointer
+      padding: 0.5em
+      color: brown
+      &:hover
+        color: white
+        background-color: brown
+    a
+      text-decoration: none
+      color: unset
+</style>
---
+++ src/views/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
+
+<template>
+  <header>
+    <section id="logo"><slot name="logo"></slot></section>
+    <section id="nav"><slot name="nav"></slot></section>
+  </header>
+</template>
+
+<style scoped lang="sass">
+header
+  width: 100%
+  background-color: orange
+  #nav
+    display: flex
+    align-items: flex-end
+    justify-content: flex-end
+    width: 100%
+</style>
