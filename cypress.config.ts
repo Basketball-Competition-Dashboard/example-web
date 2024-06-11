ee240214ecd5718eb593081a303adc0e3e9b5d71
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-06-11 20:23:47 +0800

    (adjust): Configure E2E tests
    NOTE: `web` is certainly one of submodules of `Project`

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  defaultCommandTimeout: 1000,
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
+    baseUrl: 'http://127.0.0.1:5000',
+    specPattern: 'cypress/e2e/**/*.test.ts',
   },
+  screenshotOnRunFailure: false,
 });
