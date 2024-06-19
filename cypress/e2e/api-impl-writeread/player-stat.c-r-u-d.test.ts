dbb441cce16d8fe7652056f758ebb2d99dfa3abf
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-06-19 22:53:54 +0800

    (feat): Only player create stat is set. I have no time

---
+++ cypress/e2e/api-impl-writeread/player-stat.c-r-u-d.test.ts
@@ -0,0 +1,75 @@
+before(() => {
+  cy.createSession();
+});
+
+describe('Create /player/stat', () => {
+  it('should respond 404 for non-exist name', () => {
+    cy.request({
+      method: 'POST',
+      url: '/api/web/player/stat',
+      body: {
+        "name": "undefined undefined",
+        "game_date": "2024-05-06",
+        "game_away_abbr": "HOU",
+        "game_home_abbr": "MEM",
+        "assist": 20,
+        "hit": 5,
+        "steal": 6,
+        "rebound": 25,
+        "free_throw": 3,
+        "score": 24
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+    });
+  });
+
+  it('should respond 404 for non-exist game_date', () => {
+    cy.request({
+      method: 'POST',
+      url: '/api/web/player/stat',
+      body: {
+        "name": "Precious Achiuwa",
+        "game_date": "0001-01-01",
+        "game_away_abbr": "HOU",
+        "game_home_abbr": "MEM",
+        "assist": 20,
+        "hit": 5,
+        "steal": 6,
+        "rebound": 25,
+        "free_throw": 3,
+        "score": 24
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+    });
+  });
+
+  it('should respond 404 for non-exist game_date', () => {
+    cy.request({
+      method: 'POST',
+      url: '/api/web/player/stat',
+      body: {
+        "name": "Precious Achiuwa",
+        "game_date": "0001-01-01",
+        "game_away_abbr": "HOU",
+        "game_home_abbr": "MEM",
+        "assist": 20,
+        "hit": 5,
+        "steal": 6,
+        "rebound": 25,
+        "free_throw": 3,
+        "score": 24
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+    });
+  });
+});
+
+before(() => {
+  cy.createSession();
+});
