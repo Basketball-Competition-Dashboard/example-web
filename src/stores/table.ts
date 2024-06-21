f6f8aa39e1e8555362653a4b554d88e54f225ec0
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-06-21 14:04:41 +0800

    (feat): Table store seems to be right

--- src/stores/table.ts
+++ src/stores/table.ts
@@ -1,43 +1,125 @@
+import { checkResponse } from '@/utils/checkResponse';
 import { defineStore } from 'pinia';
 
-interface RecordType {
+export interface RecordTypeNew {
   [key: string]: unknown;
+}
+
+export interface RecordType extends RecordTypeNew {
   id: number;
 }
 
-interface TableState {
+export interface TableState {
   editModes: ('Create' | 'Delete' | 'Save' | 'Update')[];
+  fields: string[];
   records: RecordType[];
+  redundantRecord?: RecordType;
+  offset: number;
+  length: number;
 }
 
 export const useTableStore = defineStore('table', {
   state: () =>
     <TableState>{
       editModes: [],
+      fields: [],
       records: [],
+      redundantRecord: undefined,
+      offset: 0,
+      length: 4,
     },
   getters: {
     getEditModes(state) {
       return state.editModes;
     },
+    getFields(state) {
+      return state.fields;
+    },
     getRecords(state) {
       return state.records;
     },
+    getOffset(state) {
+      return state.offset;
+    },
+    getLength(state) {
+      return state.length;
+    },
   },
   actions: {
-    switchCreateRecord() {
-      console.log('Creating record');
+    setRecords(records: RecordType[]) {
+      this.editModes = new Array(records.length).fill('Update');
+      this.records = records;
     },
-    switchDeleteRecord() {
-      console.log('Deleting record');
-      this.editModes = this.editModes.fill(
-        this.editModes.findIndex((mode) => mode === 'Delete') < 0
-          ? 'Delete'
-          : 'Update',
-      );
+    async readRemoteRecords(
+      call: (offset: number, length: number) => Promise<Response>,
+    ) {
+      const response = await call(this.offset, this.length);
+      if (!(await checkResponse(response))) {
+        return;
+      }
+      const records = await response.json();
+      this.setRecords(records);
     },
-    editRecord(index: number, record: RecordType) {
+    async createRemoteRecord(
+      call: (record: RecordTypeNew) => Promise<Response>,
+    ) {
+      const record = this.records[0]!;
+      const response = await call(record);
+      if (!(await checkResponse(response))) {
+        return;
+      }
+      const newRecord: RecordType = await response.json();
+      this.records[0] = newRecord;
+      this.commitEditRecord(0);
+    },
+    async updateRemoteRecord(
+      index: number,
+      call: (record: RecordType) => Promise<Response>,
+    ) {
+      const record = this.records[index];
+      if (!record) {
+        return;
+      }
+      const filtedRecord: RecordType = { id: record.id };
+      for (const field of this.fields) {
+        const fieldValue = record[field];
+        if (typeof fieldValue !== 'undefined') {
+          filtedRecord[field] = fieldValue;
+        }
+      }
+      const response = await call(filtedRecord);
+      if (!(await checkResponse(response))) {
+        return;
+      }
+      this.commitEditRecord(index);
+    },
+    async deleteRemoteRecord(
+      index: number,
+      deleteCall: (id: number) => Promise<Response>,
+      readCall: (offset: number, length: number) => Promise<Response>,
+    ) {
+      {
+        const record = this.records[index];
+        if (!record) {
+          return;
+        }
+        const response = await deleteCall(record.id);
+        if (!(await checkResponse(response))) {
+          return;
+        }
+      }
+      {
+        const response = await readCall(this.offset + index, 1);
+        if (!(await checkResponse(response))) {
+          return;
+        }
+        const record = await response.json();
+        this.records[index] = record;
+      }
+    },
+    async commitEditRecord(index: number) {
       const mode = this.editModes[index];
+      const record = this.records[index];
       if (mode === 'Delete') {
         console.log('Deleting record:', record);
         return;
@@ -53,9 +135,24 @@ export const useTableStore = defineStore('table', {
         return;
       }
     },
-    setRecords(records: RecordType[]) {
-      this.records = records;
-      this.editModes = new Array(records.length).fill('Update');
+    toggleCreateEditMode() {
+      if (this.redundantRecord) {
+        console.log('Uncreating record');
+        this.records.shift();
+        this.records.push(this.redundantRecord);
+        this.redundantRecord = undefined;
+      } else {
+        console.log('Creating record');
+        this.redundantRecord = this.records.pop();
+        this.records.unshift(<RecordType>{});
+      }
+    },
+    toggleDeleteEditMode() {
+      this.editModes = this.editModes.fill(
+        this.editModes.findIndex((mode) => mode === 'Delete') < 0
+          ? 'Delete'
+          : 'Update',
+      );
     },
   },
 });
