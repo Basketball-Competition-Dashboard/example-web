5f76d4eec9985fb6e14fd48230688084ae9d683c
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-06-24 16:51:29 +0800

    (adjust): router.ts: Add views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import GameView from '@/views/GameView.vue';
+import PlayerProfile from '@/views/PlayerProfile.vue';
+import PlayerStat from '@/views/PlayerStat.vue';
 import TeamView from '@/views/TeamView.vue';
 
 const router = createRouter({
@@ -9,84 +11,22 @@ const router = createRouter({
       path: '/',
       name: '最新賽事',
       component: TeamView,
-      meta: {},
     },
     {
       path: '/games',
       component: GameView,
     },
     {
-      path: '/teams',
-      component: TeamView,
-    },
-    {
       path: '/player-profiles',
-      name: '球員介紹',
-      component: TeamView,
-      meta: {
-        tableHeaders: {
-          name: 'PLAYER',
-          team_name: 'TEAM',
-          position: 'POSITION',
-          birthdate: 'BIRTHDATE',
-          height: 'HEIGHT',
-          weight: 'WEIGHT',
-          country: 'COUNTRY',
-        },
-        tableFieldsNew: [
-          'name',
-          'team_name',
-          'position',
-          'birthdate',
-          'height',
-          'weight',
-          'country',
-        ],
-        tableFieldsPatch: [
-          'name',
-          'team_name',
-          'position',
-          'birthdate',
-          'height',
-          'weight',
-          'country',
-        ],
-      },
+      component: PlayerProfile,
     },
     {
       path: '/player-stats',
-      name: '比賽表現',
+      component: PlayerStat,
+    },
+    {
+      path: '/teams',
       component: TeamView,
-      meta: {
-        tableHeaders: {
-          player: 'PLAYER',
-          game: 'GAME',
-          assist: 'ASSIST',
-          hit: 'HIT',
-          steal: 'STEAL',
-          rebound: 'REBOUND',
-          free_throw: 'FREE THROW',
-          score: 'SCORE',
-        },
-        tableFieldsNew: [
-          'player',
-          'game',
-          'assist',
-          'hit',
-          'steal',
-          'rebound',
-          'free_throw',
-          'score',
-        ],
-        tableFieldsPatch: [
-          'assist',
-          'hit',
-          'steal',
-          'rebound',
-          'free_throw',
-          'score',
-        ],
-      },
     },
   ],
 });
