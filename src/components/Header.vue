0d2abae1f994b76735d9500eca5bda8b956f2e74
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-06-25 23:58:59 +0800

    (adjust): Header.vue: Uses equivalent flex layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,19 @@
 <template>
-  <div class="header-vue">
-    <header class="grid">
-      <img
-        alt="Logo with a basketball"
-        id="logo"
-        src="@/assets/logo.png" />
-      <h1 id="title">籃球戰情室</h1>
-      <button id="auth">Logout</button>
-    </header>
-  </div>
+  <header class="header-vue">
+    <img
+      alt="Logo with a basketball"
+      id="logo"
+      src="@/assets/logo.png" />
+    <h1 id="title">籃球戰情室</h1>
+    <button id="auth">Logout</button>
+  </header>
 </template>
 
 <style scoped lang="sass">
-.grid
-  display: grid
-  grid-template-areas: "logo title auth"
-  grid-template-columns: min-content auto min-content
-  grid-template-rows: min-content
-  align-items: center
-
-  #logo
-    grid-area: logo
-
-  #title
-    grid-area: title
-
-  #auth
-    grid-area: auth
-
 header
   background-color: #ffc368
+  display: flex
+  align-items: center
 
   #logo
     aspect-ratio: 160 / 145
@@ -43,13 +27,14 @@ header
     font-size: 5em
     font-weight: 700
     min-width: max-content
+    width: 100%
 
   button#auth
     color: #ffffff
     cursor: pointer
     font-size: 2.25em
-    text-decoration: underline
     padding-inline: 1em
+    text-decoration: underline
 
   ::selection
     background-color: #ffad33
