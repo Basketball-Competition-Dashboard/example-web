963664b012b8c1eadc96b4adb42ad76e4f46624f
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-06-26 10:26:06 +0800

    (feat): LatestGameView.vue: Add YouTube iframe playlist - Being compatible with the home grid layout
    (adjust): LatestGameView.vue: Organize function (getOrderPair)

--- src/views/LatestGameView.vue
+++ src/views/LatestGameView.vue
@@ -3,6 +3,7 @@ import { computed, onMounted, ref } from 'vue';
 import { getGames, type Game } from '@/generated/web-api';
 import { Alert } from '@/functions/alert';
 
+const playlistId = 'PL7B_aulXoMBN03877ABcI8eYG5JsmCDjC';
 const latestGames = ref<Game[] | undefined>();
 const latestGameInfos = computed(() => {
   return (
@@ -26,7 +27,7 @@ const latestGameInfos = computed(() => {
 onMounted(async () => {
   try {
     latestGames.value = await getGames({
-      pageLength: 10,
+      pageLength: 9,
       pageOffset: 0,
       sortField: 'date',
       sortOrder: 'descending',
@@ -35,6 +36,11 @@ onMounted(async () => {
     Alert.showFailure('Read', error);
   }
 });
+
+function getOrderPairFromIndex(index: number): [number, number] {
+  const firstOrder = (index << 1) | (index & 1);
+  return [firstOrder, firstOrder ^ 1];
+}
 </script>
 
 <template>
@@ -42,7 +48,7 @@ onMounted(async () => {
     <section class="top">
       <h1 class="title">{{ $route.name }}</h1>
     </section>
-    <section class="center">
+    <section class="bottom">
       <div class="game-news">
         <template
           v-for="(gameInfo, index) of latestGameInfos"
@@ -50,13 +56,13 @@ onMounted(async () => {
           :id="index">
           <div
             class="game-info"
-            :style="{ order: (index << 1) + (index & 1) }">
-            <span class="game-date">{{ gameInfo.date }}</span>
-            <span class="game-team">{{ gameInfo.team.text }}</span>
+            :style="{ order: getOrderPairFromIndex(index)[0] }">
+            <span class="game-desc-main">{{ gameInfo.date }}</span>
+            <span class="game-desc-sub">{{ gameInfo.team.text }}</span>
           </div>
           <div
             class="game-arena"
-            :style="{ order: (index << 1) + ((index & 1) ^ 1) }">
+            :style="{ order: getOrderPairFromIndex(index)[1] }">
             <img
               alt="game-arena-background"
               class="background"
@@ -72,6 +78,32 @@ onMounted(async () => {
               :src="`/vendors/sportslogos.net/${gameInfo.team.away}.png`" />
           </div>
         </template>
+        <div
+          class="game-info"
+          :style="{
+            order: getOrderPairFromIndex(latestGameInfos.length)[0],
+          }">
+          <span class="game-desc-main">2023 ... 2024</span>
+          <span class="game-desc-sub">精彩回顧</span>
+        </div>
+        <div
+          class="game-arena"
+          :style="{
+            order: getOrderPairFromIndex(latestGameInfos.length)[1],
+          }">
+          <img
+            alt="game-arena-background"
+            class="background"
+            style="opacity: 0"
+            src="@/assets/game-arena-background.jpg"
+            sizes="1346x860" />
+          <iframe
+            allow="fullscreen"
+            class="playlist"
+            loading="lazy"
+            name="youtube-playlist"
+            :src="`https://www.youtube.com/embed?listType=playlist&list=${playlistId}&autoplay=1&controls=1&playsinline=1`"></iframe>
+        </div>
       </div>
     </section>
   </div>
@@ -103,7 +135,7 @@ onMounted(async () => {
     width: 100%
     text-align: center
 
-.center
+.bottom
   align-items: center
   display: flex
   font-size: 2.5em
@@ -121,7 +153,7 @@ onMounted(async () => {
     padding-block-end: 1.5em
     width: 70%
 
-    & > *, .game-arena .background
+    & > *
       width: 100%
 
     .game-arena
@@ -133,11 +165,11 @@ onMounted(async () => {
       flex-direction: column
       gap: 0.5em
 
-      .game-date
+      .game-desc-main
         font-size: 0.9em
         font-weight: 500
 
-      .game-team
+      .game-desc-sub
         font-size: 0.8em
         font-weight: 400
 
@@ -148,10 +180,10 @@ onMounted(async () => {
         aspect-ratio: 1346 / 860
         filter: brightness(1.2) contrast(1.2) opacity(0.7)
         position: relative
+        width: 100%
         z-index: 0
 
       .team-logo
-        border-radius: 25%
         bottom: 0
         margin-block: auto
         position: absolute
@@ -164,4 +196,9 @@ onMounted(async () => {
 
         &.away
           right: 5%
+
+      .playlist
+        height: 100%
+        position: absolute
+        width: 100%
 </style>
